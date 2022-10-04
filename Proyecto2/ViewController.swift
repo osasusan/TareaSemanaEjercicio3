a6beacfcf2c7685276b30487e8d0c2cc30ffec87
osasusan <107266592+osasusan@users.noreply.github.com>
2022-10-04 19:27:23 +0200

    Initial Commit

---
+++ Proyecto2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proyecto2
+//
+//  Created by Osasu sanchez on 4/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
